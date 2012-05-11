d55fc5cf5f7025a192494da796d932350f0184ef
James Hall <rio@x5g.com>
2012-05-12 00:00:30 +0100

    Early days

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require_once('cooke.php'); ?><!DOCTYPE html>
+<html>
+<head>
+	<title>This is an example</title>
+</head>
+<body>
+	<h1>This is a test</h1>
+	<p>This is a a test page. We've set Google Analytics and a PHP cookie</p>
+</body>
+</html>
